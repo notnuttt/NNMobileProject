b7e69be444921324601de86b5de3f35d8b1b6424
Pawan Panphinit <panphinit@gmail.com>
2018-11-25 17:48:36 +0700

    เพิ่ม class Person เพื่อให้จัดการได้ง่ายขึ้น

--- app/src/main/java/joenut/myapplication/Person.java
+++ app/src/main/java/joenut/myapplication/Person.java
@@ -1,4 +1,31 @@
 package joenut.myapplication;
 
 public class Person {
+    private String name;
+    private byte[] image;
+
+    public Person(String name, byte[] image){
+        this.name = name;
+        this.image = image;
+    }
+
+    public Person(byte[] image){
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
 }

327dd02d4295628d3c422dd1b877f8f8d76cb090
Pawan Panphinit <panphinit@gmail.com>
2018-11-27 03:06:23 +0700

    fix deleteAll function & add new method: isEmpty

--- app/src/main/java/joenut/myapplication/Database.java
+++ app/src/main/java/joenut/myapplication/Database.java
@@ -15,33 +15,34 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Database extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "database";
     private static final String TABLE_NAME = "person";
-    private static final String COLUMN_0 = "id";
-    private static final String COLUMN_1 = "name";
-    private static final String COLUMN_2 = "img";
+    private static final String COLUMN_1 = "id";
+    private static final String COLUMN_2 = "name";
+    private static final String COLUMN_3 = "img";
 
 
-    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" + COLUMN_0 + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_1 + " TEXT NOT NULL, " + COLUMN_2 + " BLOB NOT NULL)";
+    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" + COLUMN_1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_2 + " TEXT NOT NULL, " + COLUMN_3 + " BLOB NOT NULL)";
 
     public Database(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db)
-    {
+    public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE);
         Log.i("Table..","Created");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
-    {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
@@ -53,8 +54,8 @@ public class Database extends SQLiteOpenHelper {
             byte[] imgbyte = new byte[fs.available()];
             fs.read(imgbyte);
             ContentValues values = new ContentValues();
-            values.put(COLUMN_1, name);
-            values.put(COLUMN_2, imgbyte);
+            values.put(COLUMN_2, name);
+            values.put(COLUMN_3, imgbyte);
             db.insert(TABLE_NAME, null, values);
             fs.close();
             return true;
@@ -65,17 +66,54 @@ public class Database extends SQLiteOpenHelper {
         }
     }
 
-    public ArrayList<Person> getPersonList(){
+    public ArrayList<Person> getPersonList() throws DatabaseException {
         ArrayList<Person> list = new ArrayList<>();
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("select * from person", null);
+        Cursor cursor = db.rawQuery("select * from " + TABLE_NAME, null);
+        if(cursor.getCount() == 0){
+            throw new DatabaseException("Nothing Found!");
+        }
         cursor.moveToFirst();
-        while (!cursor.isAfterLast()){
-            Person p  = new Person(cursor.getString(1), cursor.getBlob(2));
+        while(!cursor.isAfterLast()){
+            String name = cursor.getString(1);
+            byte[] image = cursor.getBlob(2);
+            Person p  = new Person(name, image);
             list.add(p);
             cursor.moveToNext();
         }
+        Collections.shuffle(list);
         return list;
+    }
+
+    public ArrayList<String> getPersonListString() throws DatabaseException {
+        ArrayList<String> list = new ArrayList<>();
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from " + TABLE_NAME, null);
+        if(cursor.getCount() == 0){
+            throw new DatabaseException("Nothing Found!");
+        }
+        cursor.moveToFirst();
+        while(!cursor.isAfterLast()){
+            String name = cursor.getString(1);
+            list.add(name);
+            cursor.moveToNext();
+        }
+        Collections.shuffle(list);
+        return list;
+    }
+
+    public boolean deleteAll() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "1", null) > 0;
+    }
+
+    public boolean isEmpty(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from " + TABLE_NAME, null);
+        if(cursor.getCount() == 0){
+            return true;
+        }
+        return false;
     }
 
 }
